6d41e1666f7ce119eb88bac27a6a9a2a0fb31d6f
kmd-shafi <shaikshafi5259@gmail.com>
2024-06-25 20:00:34 +0530

    practice

---
+++ Chapter-02/practice.c
@@ -0,0 +1,65 @@
+// given num is divided by 2 or not
+
+// # include <stdio.h>
+// int main(){
+    // 1= true or even 0 = false or odd
+//     int x=10;
+//     printf("if the given number is divided by 2 or not: %d",x%2==0);
+//     return 0;
+// }
+
+
+///////////////////////////////////////////////////////////////
+
+// # include <stdio.h>
+// int main(){
+//     int sunday=1;
+//     int snowing=1;
+//     printf("%d",sunday&&snowing);
+//     return 0;
+// }
+
+
+// # include <stdio.h>
+// int main(){
+//     int sunday=1;
+//     int snowing=0;
+//     printf("%d",sunday||snowing);
+//     return 0;
+// }
+
+
+// # include <stdio.h>
+// int main(){
+//     int n;
+//     printf("Enter the number:");
+//     scanf("%d",&n);
+//     printf("%d",9 < n && n< 100);
+//     return 0;
+// }
+
+// # include <stdio.h>
+// int main(){
+//     int a=10;
+//     int b=20;
+//     int c=30;
+//     printf("Average of %d,%d,%d is %d",a,b,c,(a+b+c)/3);
+//     return 0;
+// }
+
+// # include <stdio.h>
+// int main(){
+//     int n;
+//     printf("enter the number:");
+//     scanf("%d",&n);
+//     printf("Enter number is digit or not %d", n>0 && n<100 );
+//     return 0;
+// }
+
+// # include <stdio.h>
+// int main(){
+//     int a=10;
+//     int b=20;
+//     printf("smallest number is %d\n",a<b?a:b ); 
+//     return 0;
+// }
