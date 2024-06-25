9d75418db2d2efb0d3650548bf7379aefd779565
kmd-shafi <shaikshafi5259@gmail.com>
2024-06-25 12:01:38 +0530

    variable

---
+++ Chapter-01/variable.c
@@ -0,0 +1,6 @@
+#include <stdio.h> // <stdio.h> is a pre-processor directive
+int main() {
+    int x = 5;  // x is a variable
+    printf("x = %d\n", x); //%d is a placeholder for the value of x
+    return 0;
+}
