d94ec7ac1b224a13a162a1e979b2691154fa9703
kmd-shafi <shaikshafi5259@gmail.com>
2024-06-25 12:14:34 +0530

    dataTypes

---
+++ Chapter-01/dataTypes.c
@@ -0,0 +1,20 @@
+// for example
+/*
+int, =2 bytes
+float,=4 bytes
+double,=8 bytes
+char,=1 byte
+etc..
+*/
+
+
+# include <stdio.h>
+int main() {
+    int a = 10;// int is a data type
+    printf("%d\n", a);              // %d is a placeholder for the value of a
+    float b=20.0; // float is a data type
+    printf("%f\n", b);             // %f is a placeholder for the value of b
+    char c = 'a'; // char is a data type
+    printf("%c\n", c);             // %c is a placeholder for the value of c
+    return 0;
+}
