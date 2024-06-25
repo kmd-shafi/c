bc568003a7fd2fb9e028371a76ad5a0550f7fa89
kmd-shafi <shaikshafi5259@gmail.com>
2024-06-25 14:29:12 +0530

    practice

---
+++ Chapter-01/practice.c
@@ -0,0 +1,81 @@
+// sum of two in-put's
+// # include <stdio.h>
+// int main() {
+//     int a,b;
+
+//     printf("Enter the first number: ");
+//     scanf("%d",&a);
+
+//     printf("Enter the second number: ");
+//     scanf("%d",&b);
+
+//     int c=a+b;
+
+//     printf("sum of %d and %d is %d",a,b,c);
+
+//     return 0;
+// }
+
+///////////////////////////////////////////
+
+// calculate area of square
+
+// # include <stdio.h>
+// int main(){
+//     int side;
+
+//     printf("Enter the side of square: ");
+//     scanf("%d",&side);
+//     printf("Area of square is %d",side*side);
+
+//     return 0;
+// }
+
+//////////////////////////////////////////////
+
+// alculate area of circle
+
+// # include <stdio.h>
+// int main(){
+//     float radius=5;
+//     printf("Area of circle is %f",3.14*(radius*radius));
+//     return 0;
+// }
+
+////////////////////////////////////////////////
+
+// calculate perimeter of rectangle
+
+// # include <stdio.h>
+// int main(){
+
+//     int sideA,sideB;
+
+//     printf("Enter the sideA of rectangle: ");
+//     scanf("%d",&sideA);
+
+//     printf("Enter the sideB of rectangle: ");
+//     scanf("%d",&sideB);
+
+//     int perimeter=2*(sideA+sideB);
+
+//     printf("Perimeter of rectangle is %d",perimeter);
+
+//     return 0;
+// }
+
+
+//////////////////////////////////////////////////
+
+// give out put of cube
+
+// # include <stdio.h>
+// int main(){
+//     int n;
+//     printf("Enter the number: ");
+//     scanf("%d",&n);
+//     printf("The cube of %d is %d",n,(n*n*n));
+//     return 0;
+// }
+
+
