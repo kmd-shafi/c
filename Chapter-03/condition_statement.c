93cd7db919d227b0aa5a3c304117d77939617563
kmd-shafi <shaikshafi5259@gmail.com>
2024-06-25 21:04:42 +0530

    condition_statement

---
+++ Chapter-03/condition_statement.c
@@ -0,0 +1,74 @@
+// # include <stdio.h>
+// int main(){
+//     int age;
+//     printf("Enter your age:");
+//     scanf("%d",&age);
+//     if(age>=18){
+//         printf("Adult");
+//     }else{
+//         printf("Not Adult");
+//     }
+//     return 0;
+// }
+
+
+// ternary operator
+
+// # include<stdio.h>
+// int main(){
+//     int age;
+//     printf("Enter your age:");
+//     scanf("%d",&age);
+//     age>=18 ? printf("Adult"): printf("Not Adult");
+//     return 0;
+// }
+
+//switch case
+
+// # include <stdio.h>
+// int main(){
+//     int day;
+//     printf("Enter the number:");
+//     scanf("%d",&day);
+//     switch(day){
+//         case 1:printf( "Monday") ;
+//         break;
+//         case 2:printf( "tuesday");
+//         break;
+//         case 3:printf( "wednesday");
+//         break;
+//         case 4:printf( "thursday");
+//         break;
+//         case 5:printf( "friday");
+//         break;
+//         case 6:printf( "saturday");
+//         break;
+//         case 7:printf( "sunday");
+//         break;
+//         default:printf( "Not a valid day");
+//         break;
+//            }
+//     return 0;
+// }
+
+
+// nested if else
+
+// # include <stdio.h>
+// int main(){
+//     int a;
+//     printf("Enter the number:");
+//     scanf("%d",&a);
+//     if(a>=0){
+//         printf("passitive number\n");
+//         if(a%2==0){
+//             printf("even number\n");
+//         }else{
+//             printf("odd number\n");
+//         }
+//     }else{
+//         printf("negative number\n");
+//     }
+//     return 0;
+// }
+
