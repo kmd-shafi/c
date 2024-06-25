529170e3836a12e68296aef64f9569f50eecf4f0
kmd-shafi <shaikshafi5259@gmail.com>
2024-06-25 12:28:53 +0530

    constants

---
+++ Chapter-01/constants.c
@@ -0,0 +1,18 @@
+// constants three types
+// 1. int = 1,2,3,0,-1,-2
+// 2. float or real numbers = 1.0,2.0,3.0
+// 3. char = '#','a','b'
+
+# include <stdio.h>
+int main() {
+    int a = 1;
+    printf("%d\n", a);
+
+    float b = 1.0;
+    printf("%f\n", b);
+
+    char c = '#';
+    printf("%c\n", c);
+
+    return 0;
+}
