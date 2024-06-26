c4025b0fd817f0d7bb6e162ad79a18a8d0cfea9a
kmd-shafi <shaikshafi5259@gmail.com>
2024-06-26 12:56:38 +0530

    loop

---
+++ Chapter-04/loop.c
@@ -0,0 +1,124 @@
+// loop 
+/*
+for
+while
+do while
+*/
+
+/*
+for(initialisation; condition; updation){
+do work
+}
+*/
+
+// # include <stdio.h>
+// int main(){
+//     for(int i=1;i<=5;i++)
+//     printf("Hello World! \n");
+//     return 0;
+// }
+
+
+////////////////////////////
+//pre increment & post increment
+
+// # include <stdio.h>
+// int main(){
+//     int a=1;
+//     printf ("%d\n",++a);
+//     printf ("%d\n",a);
+//     return 0;
+// }
+
+
+// # include <stdio.h>
+// int main(){
+//     int a=1;
+//     printf ("%d\n",a++);
+//     printf ("%d\n",a);
+//     return 0;
+// }
+
+////////////////////////////////////
+
+// we can use in float and char also
+// # include <stdio.h>
+// int main(){
+//     float flo=5.0;
+//     for(float i=1.0; i<=flo; i++){
+//         printf("%f\n",i);
+//     }
+//     return 0;
+// }
+
+
+// # include <stdio.h>
+// int main(){
+
+//     for(char ch='a'; ch<='z'; ch++){
+//         printf(" %c ",ch);
+//     }
+//     return 0;
+// }
+
+////////////////////////////////////////////////////
+
+/*
+initialisation
+while( condition){
+do work
+updation
+}
+*/
+
+// # include <stdio.h>
+// int main(){
+//     int i=0;
+//     while(i<=5){
+//         printf("%d\n",i);
+//         i++;
+//     }
+//     return 0;
+// }
+
+// #include <stdio.h>
+// int main(){
+//     int n;
+//     printf("Enter the number :");
+//     scanf("%d",&n);
+//     int i=0;
+//     while(i<=n){
+//         printf("%d\n",i);
+//         i++;
+//     }
+//     return 0;
+// }
+
+// # include <stdio.h>
+// int main(){
+//     int n;
+//     printf("Enter the number :");
+//     scanf("%d",&n);
+//     for(int i=0; i<=n ; ++i){
+//         printf("%d\n",i);
+//     }
+//     return 0;
+// }
+////////////////////////////////////////
+/*
+initialisation
+do{
+do work
+updation
+}while(condition)
+*/
+
+// # include <stdio.h>
+// int main(){
+//     int i=0;
+//     do{
+//         printf("%d\n",i);
+//         i++;
+//     }while(i<=5);
+//     return 0;
+// }
