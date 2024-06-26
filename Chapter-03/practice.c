ceb963bba8434cb74729e13c195ad96cc57aec39
kmd-shafi <shaikshafi5259@gmail.com>
2024-06-26 10:56:09 +0530

    practice

---
+++ Chapter-03/practice.c
@@ -0,0 +1,51 @@
+// # include <stdio.h>
+// int main(){
+//     int marks;
+//     printf("enter the marks(1 to 100): ");
+//     scanf("%d",&marks);
+//     if(marks>30 && marks<=100){
+//     printf("pass");
+//     }else if(marks<=30 && marks>=0){
+//         printf("fail");
+//     }else{
+//         printf("invalid marks");
+//     }
+//     return 0;
+// }
+
+// # include <stdio.h>
+// int main(){
+//     printf("enter marks (1 to 100): ");
+//     int marks;
+//     scanf("%d",&marks);
+//     if(marks<30){
+//         printf("C");
+//     }else if(30 <= marks&& marks< 70){
+//         printf("B");
+//     }else if(70<=marks && marks<90){
+//         printf("A");
+//     }else if(90<= marks && marks<=100){
+//         printf("A+");
+//     }
+//     return 0;
+// }
+
+///////////////////////////////
+
+// # include <stdio.h>
+// int main(){
+//     char character;
+//     printf("enter the character: ");
+//     scanf(" %c",&character);
+//     if(character>='a'&& character<='z'){
+//         printf("Lowercase");
+//     }else if (character >='A' && character<='Z'){
+//         printf("Uppercase");
+//     }else{
+//         printf("Invaild character");
+//     }
+//     return 0;
+// }
+
+///////////////////////////////////////
+
